61d7b1e8f0b5363349bab837693653b304f68f1f
gabrieljreed <gabrieljreed@gmail.com>
2022-04-11 14:10:04 -0600

    Init textRendering

--- Glitter/Glitter/Sources/main.cpp
+++ Glitter/Glitter/Sources/main.cpp
@@ -150,9 +150,9 @@ int main(int argc, char * argv[]) {
             iter->second.Draw(sceneShader);
         }
 
-        /*textShader.use();
+        textShader.use();
         RenderText(textShader, "Test", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-        sceneShader.use();*/
+        sceneShader.use();
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
